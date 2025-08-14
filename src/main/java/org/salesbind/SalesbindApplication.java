2495b68ed33b4d20d661bd78a39451153c410516
Guilherme Oliveira <oliveiraguilherme.pro@gmail.com>
2025-08-14 20:10:58 -0300

    Initial commit

---
+++ src/main/java/org/salesbind/SalesbindApplication.java
@@ -0,0 +1,13 @@
+package org.salesbind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesbindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesbindApplication.class, args);
+    }
+
+}
