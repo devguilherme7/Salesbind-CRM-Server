a99a601850405f59d86a25b2c0d283b09ac5cedd
Guilherme Oliveira <oliveiraguilherme.pro@gmail.com>
2025-08-20 21:37:57 -0300

    Add Javadoc to AbstractPersistableCustom and AggregateRoot

--- src/main/java/org/salesbind/infrastructure/persistence/jpa/entity/AbstractPersistableCustom.java
+++ src/main/java/org/salesbind/infrastructure/persistence/jpa/entity/AbstractPersistableCustom.java
@@ -12,6 +12,19 @@ import jakarta.persistence.Transient;
 
 import org.springframework.data.domain.Persistable;
 
+/**
+ * An abstract base class for JPA entities that provides an implementation of
+ * {@link Persistable} interface.
+ * <p>
+ * This class helps to optimize the behaviour of Spring Data JPA's {@code save()} method.
+ * By implementing {@link Persistable#isNew()}, we can explicitly tell the persistence
+ * provider whether an entity is new or not, avoiding an extra database {@code SELECT}
+ * statement to check for the existence of the entity before an {@code INSERT}.
+ * </p>
+ * The state is managed by a transient {@code isNew} flag, which is set fo {@code false}
+ * after the entity is first persisted or when it is loaded from the database, using
+ * JPA lifecycle callbacks ({@link PrePersist} and {@link PostLoad}).
+ */
 @MappedSuperclass
 public abstract class AbstractPersistableCustom implements Persistable<UUID> {
 
